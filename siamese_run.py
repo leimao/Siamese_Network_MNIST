d9256da5d7dd0fa530fff16f9e7e368c6a1ec58e
malekinho8 <shmeek8@gmail.com>
2023-05-18 14:34:00 -0400

    With help of ChatGPT, refactored tensorflow code to use pytorch instead

--- siamese_run.py
+++ siamese_run.py
@@ -1,42 +1,59 @@
-from tensorflow.examples.tutorials.mnist import input_data
-import tensorflow as tf
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from siamese_simple import *
+import torch
 import matplotlib.pyplot as plt
-from siamese_simple import Siamese
 
 EPISODE_MAX = 50000
 BATCH_SIZE = 128
 
-def train_model(model, dataset):
-    # Train model
-    for episode in range(EPISODE_MAX):
-        input_1, label_1 = dataset.train.next_batch(BATCH_SIZE)
-        input_2, label_2 = dataset.train.next_batch(BATCH_SIZE)
-        label = (label_1 == label_2).astype('float')
+# Preparing MNIST dataset
+train_dataset = datasets.MNIST(root='./mnist_data/', train=True, transform=transforms.ToTensor(), download=True)
+test_dataset = datasets.MNIST(root='./mnist_data/', train=False, transform=transforms.ToTensor(), download=True)
 
-        train_loss = model.train_model(input_1 = input_1, input_2 = input_2, label = label)
-
-        if episode % 20 == 0:
-            print('episode %d: train loss %.3f' % (episode, train_loss))
-
-        if episode % 1000 == 0:
-            model.save_model()
+train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
 
-def test_model(model, dataset):
-    # Test model
-    embed = model.test_model(input_1 = dataset.test.images)
-    embed.tofile('embed.txt')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+siamese_net = Siamese().to(device)
+criterion = ContrastiveLoss()
+optimizer = torch.optim.SGD(siamese_net.parameters(), lr=LEARNING_RATE)
 
-def main():
-    # Load MNIST dataset
-    mnist = input_data.read_data_sets('MNIST_data', one_hot = False)
-    # Initialze model
-    siamese = Siamese()
-    # Train model
-    train_model(model = siamese, dataset = mnist)
-    # Test model
-    test_model(model = siamese, dataset = mnist)
+# Dictionary for storing losses
+losses = {
+    'train': [],
+    'test': [],
+}
 
-if __name__ == '__main__':
+# Adjusted training loop with loss storage
+for episode in range(EPISODE_MAX):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.view(-1, 28*28).to(device)
+        labels = labels.to(device)
+        pairs, targets = create_pairs(images, labels)
 
-    main()
+        # Forward pass
+        output1, output2 = siamese_net(pairs[:, 0], pairs[:, 1])
+        loss = criterion(output1, output2, targets)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if (i+1) % 20 == 0:
+            print('Episode [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(episode+1, EPISODE_MAX, i+1, len(train_loader), loss.item()))
+
+    # Store training loss for this episode
+    losses['train'].append(loss.item())
+
+    # Compute test loss
+    test_loss = compute_test_loss(siamese_net, test_loader, criterion, device)
+    losses['test'].append(test_loss)
+
+    print('Episode [{}/{}], Test Loss: {:.4f}'.format(episode+1, EPISODE_MAX, test_loss))
+
+# Save model
+if (episode+1) % 1000 == 0:
+    save_model()
